f33079ba151a16267b1cbffbfc95c776756d3cdb
Leonardo Vidal <leonardolinsvv17@gmail.com>
2025-09-03 16:54:19 -0300

    alterando o cabecalho2

--- test.py
+++ test.py
@@ -20,8 +20,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/')
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(),
-                         "Pipeline DevOps - Leonardo Vidal.")
+        self.assertRegex(result.data.decode(),"Pipeline DevOps - Leonardo Vidal.")
 
 
 if __name__ == "__main__":
